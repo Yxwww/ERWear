14bf27c973bd2057d8f04591de314cf5febbab1a
YuxiBro <yuxi.wangcd@gmail.com>
2016-03-25 00:52:42 -0600

    init commit

---
+++ HelloGlassWorld/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Glass.App;
+using Android.Glass.Widget;
+using SoD_Xamarin_AndroidLibrary;
+
+namespace HelloGlassWorld
+{
+	[Activity (Label = "Hello Xamarin", Icon = "@drawable/Icon", MainLauncher = true, Enabled = true)]
+	[IntentFilter (new String[]{ "com.google.android.glass.action.VOICE_TRIGGER" })]
+	[MetaData ("com.google.android.glass.VoiceTrigger", Resource = "@xml/voicetriggerstart")]
+	public class MainActivity : Activity
+	{
+		SoD sod = new SoD("10.13.65.16",3000,AndroidDeviceType.Glass);
+		// The project requires the Google Glass Component from
+		// https://components.xamarin.com/view/googleglass
+		// so make sure you add that in to compile succesfully.
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+            var b = new CardBuilder (this, CardBuilder.Layout.Text);
+            b.SetText ("Welcome to Xamarin Google Glass Development");
+            b.SetFootnote ("Let's get hacking!");
+            b.SetTimestamp ("now");
+
+            SetContentView (b.View);
+		}
+	}
+}
+
+
---
+++ SoD_Xamarin_AndroidLibrary/SoD.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Xamarin.Socket.IO;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+namespace SoD_Xamarin_AndroidLibrary
+{
+	public class SoD
+	{
+		SocketIO socket;	
+		public AndroidDeviceType deviceType;
+		public SoD (string host,int port, AndroidDeviceType type)
+		{	
+			socket = new SocketIO (host, port);
+			this.deviceType = type;
+			register ();
+		}
+		public async void register(){
+			var connectionStatus = await socket.ConnectAsync ();
+
+			if (connectionStatus == SocketIO.ConnectionStatus.Connected) {
+				//registerAsAndroidWatch ();
+				switch (this.deviceType) {
+				case AndroidDeviceType.Glass:
+					registerAsGoogleGlass ();
+					break;
+				case AndroidDeviceType.Watch:
+					registerAsAndroidWatch();
+					break;
+				default:
+					Console.WriteLine ("Unkonwn type: " + this.deviceType);
+					break;
+				}
+
+			} else {
+				Console.WriteLine ("Websocket failed to connect to the server");
+			}
+		}
+		public void test(){
+			//var list = new object [] { 1, "randomString", 3.4f, new Foo () { Bar = "baz"} };
+			//Foo aFoo = new Foo (){ Bar = "baz" };
+
+		}
+		public void registerAsAndroidWatch(){
+			socket.Emit ("registerDevice", new Object[] {new AndroidWatch () {
+					name = "Android Watch",
+					deviceType = "AndroidWatch",
+					width = 0.1,
+					height=0.1,
+					depth = 0.1,
+					stationary = false
+				} });
+		}
+		public void registerAsGoogleGlass(){
+			socket.Emit ("registerDevice", new Object[] {new GoogleGlass () {
+					name = "GoogleGlass",
+					deviceType = "Glass",
+					width = 0.1,
+					height=0.1,
+					depth = 0.1,
+					stationary = false
+				} });
+		}
+	}
+
+
+	[JsonObject(MemberSerialization.OptIn)]
+	public class AndroidWatch: AndroidWear
+	{
+		[JsonProperty]
+		public string name { get; set;} 
+		[JsonProperty]
+		public string deviceType { get; set;} 
+		[JsonProperty]
+		public double width { get; set;} 
+		[JsonProperty]
+		public double height { get; set;} 
+		[JsonProperty]
+		public double depth { get; set;} 
+		[JsonProperty]
+		public bool stationary {get;set;}
+	}
+	[JsonObject(MemberSerialization.OptIn)]
+	public class GoogleGlass: AndroidWear
+	{
+		[JsonProperty]
+		public string name { get; set;} 
+		[JsonProperty]
+		public string deviceType { get; set;} 
+		[JsonProperty]
+		public double width { get; set;} 
+		[JsonProperty]
+		public double height { get; set;} 
+		[JsonProperty]
+		public double depth { get; set;} 
+		[JsonProperty]
+		public bool stationary {get;set;}
+	}
+	public interface AndroidWear{
+		[JsonProperty]
+		string name { get; set;} 
+		[JsonProperty]
+		string deviceType { get; set;} 
+		[JsonProperty]
+		double width { get; set;} 
+		[JsonProperty]
+		double height { get; set;} 
+		[JsonProperty]
+		double depth { get; set;} 
+		[JsonProperty]
+		bool stationary {get;set;}
+	}
+	public enum AndroidDeviceType{
+		Glass,Watch
+	}
+}
+
